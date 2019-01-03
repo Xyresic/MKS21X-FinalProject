548b5e51e6170f12948532d8e95c4a8e9e140adb
Kevin Chen <kchen01@stuy.edu>
2019-01-03 18:41:24 -0500

    tried scanner

---
+++ test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class test{
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    System.out.println("Enter a Expression:");
+    double expression = reader.nextDouble();
+    System.out.println(expression);
+  }
+}

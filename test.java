0db4dd437d897c4ca035693c373a2ac1deaf8e5d
Kevin Chen <kchen01@stuy.edu>
2019-01-03 20:23:59 -0500

    added a lot of stuff

--- test.java
+++ test.java
@@ -2,26 +2,43 @@
 import java.util.*;
 
 public class test{
+  char[] operations = {'+','-','*','/'};
+  public boolean isOperator(char input) {
+    for (char c : operations) {
+      if (c == input) {
+        return true;
+      }
+    }
+    return false;
+  }
   double result = 9.12;
-  public static void Splitter(String input){
-      ArrayList<String> output = new ArrayList<String>();
-      ArrayList<String> stacks = new ArrayList<String>();
+  public void Splitter(String input){
+      ArrayList<String> output;
+      ArrayList<String> stacks;
       int i = 0;
       while (i < input.length()) {
         // case senario for no spaces
         String holder = "";
         int peroidcount = 0;
+        if (Character.isDigit(input.charAt(i)) || input.charAt(i) == '.') {
         while((Character.isDigit(input.charAt(i)) || input.charAt(i) == '.')  && peroidcount < 2 && i < input.length()) {
           if (input.charAt(i) == '.') {
             peroidcount += 1;
           }
           holder += input.charAt(i);
           i += 1;
-
         }
         output.add(holder);
-        System.out.println(output);
-        i = input.length();
+      }
+
+     if (isOperator(input.charAt(i))) {
+
+        }
+      }
+
+
+
+        output.add(holder);
       }
 
 

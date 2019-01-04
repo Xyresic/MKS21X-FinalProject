915cafee6365647e02489d70b8397bdd70613335
Kevin Chen <kchen01@stuy.edu>
2019-01-03 19:23:55 -0500

    found a way to token my string :)

--- test.java
+++ test.java
@@ -1,9 +1,40 @@
-import java.util.Scanner;
+
+import java.util.*;
+
 public class test{
+  double result = 9.12;
+  public static void Splitter(String input){
+      ArrayList<String> output = new ArrayList<String>();
+      ArrayList<String> stacks = new ArrayList<String>();
+      int i = 0;
+      while (i < input.length()) {
+        // case senario for no spaces
+        String holder = "";
+        int peroidcount = 0;
+        while((Character.isDigit(input.charAt(i)) || input.charAt(i) == '.')  && peroidcount < 2 && i < input.length()) {
+          if (input.charAt(i) == '.') {
+            peroidcount += 1;
+          }
+          holder += input.charAt(i);
+          i += 1;
+
+        }
+        output.add(holder);
+        System.out.println(output);
+        i = input.length();
+      }
+
+
+
+  }
+
+
   public static void main(String[] args) {
+    String fourfunctiontester = "3.12+6";
     Scanner reader = new Scanner(System.in);
     System.out.println("Enter a Expression:");
     double expression = reader.nextDouble();
-    System.out.println(expression);
+    Splitter(fourfunctiontester);
+
   }
 }

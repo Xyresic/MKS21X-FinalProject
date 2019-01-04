83aa1829a718d7bdd963586445978f83a212538c
Eric Lam <elam00@stuy.edu>
2019-01-04 14:30:19 -0500

    Created Main Class, Added Constants

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator{
+  public static double pi = 3.141592653589793;
+  public static double e = 2.718281828459045;
+  public static double phi = 1.618033988749895;
+  public static void main(String[] args){
+  }
+}

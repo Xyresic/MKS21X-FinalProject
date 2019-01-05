c5514f32f9018949bbeb1205fd4400f7a0b7448f
Eric Lam <elam00@stuy.edu>
2019-01-05 14:10:52 -0500

    Finished Store Method

--- Calculator.java
+++ Calculator.java
@@ -1,19 +1,19 @@
+import java.util.*;
 public class Calculator{
   public static double pi = 3.141592653589793;
   public static double e = 2.718281828459045;
   public static double phi = 1.618033988749895;
-  private char[] storedChar = new char[26];
   private double[] storedVal = new double[26];
-  for(char c : storedChar){
-
-  }
   public void store(char name, double value){
-
+    if((int)name<65 || (int)name>90){
+      throw new IllegalArgumentException("Please use a capital letter.");
+    }
+    storedVal[name-65]=value;
   }
   public static void help(){
   }
   public static void main(String[] args){
-    if(args.length == 1 && !args[0].contains('=')){
+    if(args.length == 1 && !args[0].contains("=")){
       Expression temp = new Expression(args[0]);
       System.out.println(temp.evaluate());
     }
